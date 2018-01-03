dfa19ff6d6bcd804f13ae148c807b2271e2521ad
Ricardo Jesus <rdo_ramone@hotmail.com>
2018-01-03 14:22:18 -0200

    Adicionando novo serviço para mostrar como injetar um serviço dentro do outro

--- curso-de-angular-loiane/servicos/src/app/app.module.ts
+++ curso-de-angular-loiane/servicos/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 
+import { LogService } from './shared/log.service';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -18,8 +20,11 @@ import { CriarCursoModule } from './criar-curso/criar-curso.module';
     Aqui nós estamos fornecendo o serviço CursosService de forma que ele possa ser usado
     em toda a aplicação, sem a necessidade de ser declarado novamente em outros módulos.
     Lembrando que não é necessário fazer o import do service declarado no metadado "providers".
+
+    Aqui colocamos o LogService para que ele seja utilizado em toda a aplicação, pois ao
+    declarar ele no AppModule ele passa a ter escopo global.
   */
-  providers: [],
+  providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
