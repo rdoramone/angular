abdb53996030730f4daca553d4bac32034fa1729
Ricardo Jesus <rdo_ramone@hotmail.com>
2018-01-17 17:29:43 -0200

    Aplicando locale nos pipes usando internacionalização

--- curso-de-angular-loiane/pipes/src/app/app.module.ts
+++ curso-de-angular-loiane/pipes/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
 
+// A alternativa para corrigir a inserção de LOCALE_ID é usar esses 2 imports e o método abaixo.
+import { registerLocaleData } from '@angular/common';
+import ptBR from '@angular/common/locales/pt';
+
+registerLocaleData(ptBR);
+
+import { SettingsService } from './settings.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +23,28 @@ import { CamelCasePipe } from './camel-case.pipe';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    /*
+      Temos 3 formas de fazer essa injeção de dependência
+
+      Podemos usar a propriedade 'useValue', para passar apenas um valor.
+
+      Podemos usar 'useClass' que é a forma mais comum de se utilizar. E para o 'useClass',
+      nós passamos o nome da Classe a ser utilizada.
+
+      Podemos usar 'useFactory' utilizando o padrão de projeto Factory.
+    */
+    /* {
+      provide: LOCALE_ID,
+      useValue: 'pt-PT'
+    }*/
+    SettingsService, {
+      provide: LOCALE_ID,
+      // Em 'deps' nós passamos os providers que esse LOCALE_ID precisam para funcionar
+      deps: [SettingsService],
+      useFactory: (settingsService) => settingsService.getLocale()
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
