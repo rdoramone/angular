91cb617e04f58b308aea86efa715d095f2801053
Ricardo Jesus <rdo_ramone@hotmail.com>
2018-01-19 17:44:27 -0200

    Definindo e extraindo parâmetros de url com queryParams

--- curso-de-angular-loiane/rotas/src/app/cursos/cursos.component.ts
+++ curso-de-angular-loiane/rotas/src/app/cursos/cursos.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { CursosService } from './cursos.service';
+
+import { Subscription } from 'rxjs/RX';
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
@@ -9,11 +13,37 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: any[];
+  pagina: number;
+  inscricao: Subscription
 
-  constructor(private cursoService: CursosService) { }
+  constructor(
+    private cursoService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
   // O ngOnInit é o melhor lugar para se carregar informações
   ngOnInit() {
     this.cursos = this.cursoService.getCursos();
+
+    this.inscricao = this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        // queryparams também é um Behavior Subject, ou seja, ele também retorna uma inscrição.
+        this.pagina = queryParams['pagina'];
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina() {
+    this.router.navigate(
+      ['/cursos'], 
+      { queryParams: 
+        { 'pagina': ++this.pagina } 
+      }
+    );
   }
 
 }
