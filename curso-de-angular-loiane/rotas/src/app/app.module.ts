a098f17564fee6b4786ead95face2975741a5056
Ricardo Jesus <rdo_ramone@hotmail.com>
2018-01-19 14:19:06 -0200

    Definindo e extraindo parâmetros de rotas

--- curso-de-angular-loiane/rotas/src/app/app.module.ts
+++ curso-de-angular-loiane/rotas/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MaterializeModule } from 'angular2-materialize';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 import { routing } from './app.routing';
 
@@ -17,7 +18,8 @@ import { routing } from './app.routing';
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
--- curso-de-angular-loiane/rotas/src/app/app.routing.ts
+++ curso-de-angular-loiane/rotas/src/app/app.routing.ts
@@ -4,10 +4,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 const APP_ROUTES: Routes = [
-    { path: 'login', component: LoginComponent },
     { path: 'cursos', component: CursosComponent },
+    /*
+        Para colocar um caminho que tenha um parâmetro no final da url, por exemplo, um id.
+        Nós colocamos sempre o ':' dois pontos, para indicar que é um parâmetro
+        e o nome do parâmetro de sua preferência.
+            Exemplo: 
+                'cursos/:id'
+    */
+    { path: 'curso/:id', component: CursoDetalheComponent },
+    { path: 'login', component: LoginComponent },
     { path: '', component: HomeComponent }
 ];
 // a propriedade 'forRoot' me mostra todas as rotas do projeto.
