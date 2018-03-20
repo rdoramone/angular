be4fdd85ce68e09b18f40963eb601d5c3ab1ea64
Ricardo Jesus <rdo_ramone@hotmail.com>
2018-03-19 21:07:03 -0300

    Populando campos do formulário usando setValue e patchValue

--- curso-de-angular-loiane/forms/src/app/template-form/template-form.component.ts
+++ curso-de-angular-loiane/forms/src/app/template-form/template-form.component.ts
@@ -29,18 +29,66 @@ export class TemplateFormComponent implements OnInit {
     };
   }
 
-  consultaCEP(cep) {
+  consultaCEP(cep, form) {
     cep.replace(/\D/g, '');
 
     if (cep !== "") {
+      this.resetaDadosForm(form);
+
       var validacep = /^[0-9]{8}$/;
 
-        if (validacep.test(cep)) {
-          this.http.get(`https://viacep.com.br/ws/${cep}/json`)
-          .map(dados => dados.json())
-          .subscribe(dados => console.log(dados));
-        }
+      if (validacep.test(cep)) {
+        this.http.get(`https://viacep.com.br/ws/${cep}/json`)
+        .map(dados => dados.json())
+        .subscribe(dados => this.populaDadosForm(dados, form));
+      }
     }
+  }
+
+  populaDadosForm(dados, formulario) {
+    /*
+      Ao utilizar o método 'setValue()' nós conseguimos popular os dados do formulário com os dados da API de CEP, mas para que os dados
+      do nome e email que já foram preenchidos, sejam preservados, nós precisamos passar os valores deles que já estavam preenchidos.
+      Caso haja um formulário grande com diversos campos teriamos que fazer isso manualmente para vários campos, mas para evitar esse
+      trabalho, nós podemos usar o método'patchValue()', com ele nós só passamos os campos no qual receberão os dados da API, sem impactar
+      os demais campos já preenchidos.
+    */
+    /*formulario.setValue({
+      nome: formulario.value.nome,
+      email: formulario.value.email,
+      endereco: {
+        rua: dados.logradouro,
+        cep: dados.cep,
+        numero: '',
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });*/
+
+    formulario.form.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        cep: dados.cep,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+  }
+
+  resetaDadosForm(formulario) {
+    formulario.form.patchValue({
+      endereco: {
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
   }
 
   onSubmit(form) {
