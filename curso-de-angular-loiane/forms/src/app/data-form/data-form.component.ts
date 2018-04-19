b966fdbb126a517cabcfc1f3f52117a1d2b88dd7
Milene Cajé <milene.caje@accenture.com>
2018-04-19 17:44:54 -0300

    Adicionando validação no formulário e corrigindo url do post

--- curso-de-angular-loiane/forms/src/app/data-form/data-form.component.ts
+++ curso-de-angular-loiane/forms/src/app/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { Http, HttpModule } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -28,13 +28,13 @@ export class DataFormComponent implements OnInit {
 
     // Essa forma é a mesma coisa da forma acima, mas de forma simplificada.
     this.formulario = this.formBuilder.group({
-      nome: [null],
-      email: [null]
+      nome: [null, [Validators.required, Validators.minLength(3)]],
+      email: [null, [Validators.required, Validators.email]]
     });
   }
 
   onSubmit() {
-    this.http.post('https://httipbin.org/post', JSON.stringify(this.formulario.value))
+    this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
       .map(res => res)
       .subscribe(dados => {
         console.log(dados);
