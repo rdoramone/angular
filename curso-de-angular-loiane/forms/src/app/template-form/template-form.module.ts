8a41a1695f6f48e1178e251b1a8207d1f53f8c7b
Milene Cajé <milene.caje@accenture.com>
2018-04-19 17:13:54 -0300

    Sincronizando o Template HTML com o FormGroup do Angular

--- curso-de-angular-loiane/forms/src/app/template-form/template-form.module.ts
+++ curso-de-angular-loiane/forms/src/app/template-form/template-form.module.ts
@@ -2,20 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { SharedModule } from '../shared/shared.module';
 import { TemplateFormComponent } from './template-form.component';
-import { FormDebugComponent } from './../form-debug/form-debug.component';
-import { CampoControlErroComponent } from './../campo-control-erro/campo-control-erro.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    SharedModule
   ],
   declarations: [
-    TemplateFormComponent,
-    FormDebugComponent,
-    CampoControlErroComponent
+    TemplateFormComponent
   ]
 })
 export class TemplateFormModule { }
