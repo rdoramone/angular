880e3ada2e22a7b36b93ffcf3c71209c2313ed87
Milene Cajé <milene.caje@accenture.com>
2018-04-19 18:04:54 -0300

    Acessando o FormControl no HTML para estilizar os campos inválidos com css

--- curso-de-angular-loiane/forms/src/app/data-form/data-form.component.ts
+++ curso-de-angular-loiane/forms/src/app/data-form/data-form.component.ts
@@ -47,4 +47,22 @@ export class DataFormComponent implements OnInit {
   resetar() {
     this.formulario.reset();
   }
+
+  verificaValidTouched(campo) {
+    return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
+  }
+
+  verificaEmailInvalido() {
+    let campoEmail = this.formulario.get('email');
+
+    if (campoEmail.errors) {
+      return campoEmail.errors['email']
+    }
+  }
+
+  aplicaCssErro(campo) {
+    return {
+      'is-invalid': this.verificaValidTouched(campo)
+    };
+  }
 }
